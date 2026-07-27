7f945da142cac33601ec1ce112e62a51a521ed26
YousefAbdElrahman17 <jolmera2003@gmail.com>
2026-07-27 17:59:07 +0300

    TwoSum Solved.

--- src/beginner/solutions/TwoSum.java
+++ src/beginner/solutions/TwoSum.java
@@ -1,4 +1,22 @@
 package beginner.solutions;
 
 public class TwoSum {
+    public static void main(String[] args){
+        //no need to print
+    }
+}
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int i,j;
+        int[] Arr = new int[2];
+        for(i=0 ; i<nums.length ; i++){
+            for(j=i+1 ; j<nums.length ; j++){
+                if(nums[i]+nums[j] == target){
+                    Arr[0] = i;
+                    Arr[1] = j;
+                }
+            }
+        }
+        return Arr;
+    }
 }
