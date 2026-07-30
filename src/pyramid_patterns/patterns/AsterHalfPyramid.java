963a1af0b91c5c356803269c7c625b2f43f25018
YousefAbdElrahman17 <jolmera2003@gmail.com>
2026-07-30 18:53:14 +0300

    add halfPyramid code .. update .gitignore .. update .iml

--- src/pyramid_patterns/patterns/AsterHalfPyramid.java
+++ src/pyramid_patterns/patterns/AsterHalfPyramid.java
@@ -1,4 +1,20 @@
 package pyramid_patterns.patterns;
 //1
 public class AsterHalfPyramid {
+    public static void main(String[] args){
+        int num = 5,row, col;
+        for(row=0 ; row<num; row++){
+            for(col=0; col<=row; col++){
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
     }
+}
+/*===============================
+ *
+ * *
+ * * *
+ * * * *
+ * * * * *
+ ================================*/
