744035b6b4264112a42fba5de194d0a1bebe29c8
YousefAbdElrahman17 <jolmera2003@gmail.com>
2026-08-22 00:23:26 +0300

    inverted pyramids with Asterisks has been added

--- src/pyramid_patterns/patterns/AsterInvertedPyramid.java
+++ src/pyramid_patterns/patterns/AsterInvertedPyramid.java
@@ -1,4 +1,28 @@
 package pyramid_patterns.patterns;
 
 public class AsterInvertedPyramid {
+
+    public static void main(String[] args){
+        int col, row, space, num=9;
+        for(row=1; row<=num; row+=2){
+            for(space=1; space<=(row); space++) System.out.print(" ");
+            for(col=num; col>=row; col-=1)     System.out.print("* ");
+            System.out.println();
+        }
+    }
 }
+           /*===============================
+                 space<=(row/2) .. "*"
+                *********
+                 *******
+                  *****
+                   ***
+                    *
+             ================================
+                  space<=(row)  .. "* "
+             * * * * * * * * *
+               * * * * * * *
+                 * * * * *
+                   * * *
+                     *
+           ================================*/
