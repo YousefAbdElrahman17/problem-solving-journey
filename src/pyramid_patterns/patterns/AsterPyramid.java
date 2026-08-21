b663dc911c0234d7aa061b2ed1b5ea31336921ef
YousefAbdElrahman17 <jolmera2003@gmail.com>
2026-08-22 00:11:26 +0300

    Add pyramids with Asterisks

--- src/pyramid_patterns/patterns/AsterPyramid.java
+++ src/pyramid_patterns/patterns/AsterPyramid.java
@@ -1,4 +1,27 @@
 package pyramid_patterns.patterns;
 
 public class AsterPyramid {
+    public static void main(String[] args){
+        int col, row, space, num=9;
+        for(row=1; row<=num; row+=2){
+            for(space=(num-row); space>=0; space--) System.out.print(" ");
+            for(col=1; col<=row; col+=1)     System.out.print("* ");
+            System.out.println();
+        }
+    }
 }
+           /*===============================
+                 (num-row)/2 .. "*"
+                 *
+                ***
+               *****
+              *******
+             *********
+             ================================
+                     num-row  .. "* "
+                     *
+                   * * *
+                 * * * * *
+               * * * * * * *
+             * * * * * * * * *
+           ================================*/
